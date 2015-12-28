7692ad4363b2934c673142289e7310631b2f266f
kenneth <kenneth@kennethbowen.com>
2015-12-28 15:32:25 -0700

    Fix number gen issue.
    Was creating bogus diceware numbers with non-dice numbers (0,7-9).
    - enhance test case
    - add netbeans BS to .gitignore

--- number-generator/src/main/java/com/cultureofcode/diceware/NumberGenerator.java
+++ number-generator/src/main/java/com/cultureofcode/diceware/NumberGenerator.java
@@ -5,46 +5,55 @@ import java.util.HashSet;
 import java.util.Set;
 
 /**
- * Generates Diceware random (enough) numbers: numbers between 11111 - 66666.
- * See http://world.std.com/~reinhold/diceware.html for more info.
+ * Generates Diceware random (enough) numbers: numbers between 11111 - 66666. See
+ * http://world.std.com/~reinhold/diceware.html for more info.
  *
- * <em>Note</em>: the creator of diceware recommends that you DO NOT use software to create
- * a passphrase, which is exactly what we are doing here. He also mentions Java's SecureRandom is
- * good enough to use.
+ * <em>Note</em>: the creator of diceware recommends that you DO NOT use software to create a passphrase, which is
+ * exactly what we are doing here. He also mentions Java's SecureRandom is good enough to use.
  */
 public class NumberGenerator {
 
-    int min = 11111;
-    int max = 66666;
+  SecureRandom rand = new SecureRandom();
 
-    /**
-     *
-     * @return a random-ish integer between 11111 and 66666
-     */
-    public Integer generate() {
+  /**
+   * Returns an integer made by throwing a dice 5 times.
+   *
+   * @return a random-ish integer between 11111 and 66666
+   */
+  public Integer generate() {
 
-        SecureRandom secureRandom = new SecureRandom();
-        return secureRandom.nextInt(max - min) + min;
+    StringBuilder sb = new StringBuilder();
+
+    for (int i = 0; i < 5; i++) {
+      sb.append(throwDice());
     }
 
-    /**
-     * Convenience for generating a set of 5 to 10 diceware numbers.
-     *
-     * @param num number to generate - must be between 5 and 10
-     * @return a <i>num</i> size Set of random integers between 11111 and 66666
-     */
-    public Set<Integer> generate(int num) {
+    return Integer.valueOf(sb.toString());
+  }
 
-        if (num < 5 || num > 10) {
-            throw new IllegalArgumentException("Can only generate between 5 & 10 diceware numbers");
-        }
+  Integer throwDice() {
 
-        Set<Integer> diceNums = new HashSet<>();
+    return rand.nextInt(6) + 1;
+  }
 
-        while (diceNums.size() < num) {
-            diceNums.add(generate());
-        }
+  /**
+   * Convenience for generating a set of 5 to 10 diceware numbers.
+   *
+   * @param num number to generate - must be between 5 and 10
+   * @return a <i>num</i> size Set of random integers between 11111 and 66666
+   */
+  public Set<Integer> generate(int num) {
 
-        return diceNums;
+    if (num < 5 || num > 10) {
+      throw new IllegalArgumentException("Can only generate between 5 & 10 diceware numbers");
     }
+
+    Set<Integer> diceNums = new HashSet<>();
+
+    while (diceNums.size() < num) {
+      diceNums.add(generate());
+    }
+
+    return diceNums;
+  }
 }
--- number-generator/src/test/java/com/cultureofcode/diceware/NumberGeneratorTest.java
+++ number-generator/src/test/java/com/cultureofcode/diceware/NumberGeneratorTest.java
@@ -1,65 +1,79 @@
 package com.cultureofcode.diceware;
 
-import java.util.Arrays;
 import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.*;
+
+import java.util.Arrays;
 import org.junit.Test;
 
 public class NumberGeneratorTest {
 
-    private int min = 11111;
-    private int max = 66666;
+  private int min = 11111;
+  private int max = 66666;
 
-    @Test
-    public void generate() {
+  @Test
+  public void generate() {
 
-        NumberGenerator numberGenerator = new NumberGenerator();
+    NumberGenerator numberGenerator = new NumberGenerator();
 
-        for (int i = 0; i < 10000; i++) {
-            Integer diceNum = numberGenerator.generate();
-            assertThat(diceNum, greaterThanOrEqualTo(min));
-            assertThat(diceNum, lessThanOrEqualTo(max));
-        }
+    for (int i = 0; i < 10000; i++) {
+      Integer diceNum = numberGenerator.generate();
+      assertThat(diceNum, greaterThanOrEqualTo(min));
+      assertThat(diceNum, lessThanOrEqualTo(max));
     }
+  }
 
-    @Test
-    public void generateValid() {
-      NumberGenerator numberGenerator = new NumberGenerator();
+  @Test
+  public void generateValid() {
+    NumberGenerator numberGenerator = new NumberGenerator();
 
-        for (int i = 0; i < 10000; i++) {
-            Integer diceNum = numberGenerator.generate();
-            String s = String.valueOf(diceNum);
-            s.chars().filter(digit -> digit == '0')
-                    .forEach(digit -> fail("Generated bogus number " + diceNum));
-            // TODO check for additional non-dice numbers (7,8,9)
-        }
+    for (int i = 0; i < 10000; i++) {
+      Integer diceNum = numberGenerator.generate();
+      String s = String.valueOf(diceNum);
+      s.chars().filter(digit -> digit == '0' || digit > '6')
+          .forEach(digit -> fail("Generated bogus number " + diceNum));
     }
+  }
 
-    @Test
-    public void generateSet() {
+  @Test
+  public void generateSet() {
 
-        NumberGenerator numberGenerator = new NumberGenerator();
+    NumberGenerator numberGenerator = new NumberGenerator();
 
-        // scalacheck would be the bomb here!
-        int[] setSize = {5,6,7,8,9,10};
-        Arrays.stream(setSize).forEach(i -> {
-            assertThat(numberGenerator.generate(i).size(), equalTo(i));
-        });
+    // scalacheck would be the bomb here!
+    int[] setSize = {5, 6, 7, 8, 9, 10};
+    Arrays.stream(setSize).forEach(i -> {
+      assertThat(numberGenerator.generate(i).size(), equalTo(i));
+    });
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void generateSetTooBig() {
+
+    NumberGenerator numberGenerator = new NumberGenerator();
+    // can you imagine?
+    numberGenerator.generate(55556);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void generateSetTooSmall() {
+
+    NumberGenerator numberGenerator = new NumberGenerator();
+    // generator pleaze...
+    numberGenerator.generate(1);
+  }
+
+  @Test
+  public void throwDice() {
+
+    NumberGenerator numberGenerator = new NumberGenerator();
+
+    for (int i = 0; i < 10000; i++) {
+      int diceNum = numberGenerator.throwDice();
+      if (diceNum < 1 || diceNum > 6) {
+        fail("Dice throw is out of range " + diceNum);
+      }
     }
+  }
 
-    @Test (expected = IllegalArgumentException.class)
-    public void generateSetTooBig() {
-
-        NumberGenerator numberGenerator = new NumberGenerator();
-        // can you imagine?
-        numberGenerator.generate(55556);
-    }
-
-    @Test (expected = IllegalArgumentException.class)
-    public void generateSetTooSmall() {
-
-        NumberGenerator numberGenerator = new NumberGenerator();
-        // generator pleaze...
-        numberGenerator.generate(1);
-    }
 }
