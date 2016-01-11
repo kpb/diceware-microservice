e90394d22470eca2be5b3659a546f493e0c2b9d6
Kenneth Bowen <kenneth@kennethbowen.com>
2016-01-10 19:55:19 -0700

    Add stub of spring boot cli project.

---
+++ diceware-cli/src/main/java/com/cultureofcode/diceware/cli/DicewareCLI.java
@@ -0,0 +1,12 @@
+package com.cultureofcode.diceware.cli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DicewareCLI {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DicewareCLI.class, args);
+	}
+}
