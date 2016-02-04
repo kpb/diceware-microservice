a4cc25f96423d734da8a487ac99fb3ae37a1bdd6
Kenneth Bowen <kenneth@kennethbowen.com>
2016-02-03 17:13:18 -0700

    cleanup cli code.

--- diceware-cli/src/main/java/com/cultureofcode/diceware/cli/DicewareCommandLineRunner.java
+++ diceware-cli/src/main/java/com/cultureofcode/diceware/cli/DicewareCommandLineRunner.java
@@ -29,6 +29,7 @@ public class DicewareCommandLineRunner implements CommandLineRunner {
         int words = promptUser();
 
         System.out.println("Generating a " + words + " word passphrase...");
+        System.out.println();
 
         // generate the passphrase
         Map<Integer, String> passphrase = dicewareService.generatePassphrase(words);
@@ -38,6 +39,9 @@ public class DicewareCommandLineRunner implements CommandLineRunner {
         System.out.println();
         passphrase.keySet().forEach(num -> System.out.printf("%-10.10s", num));
         System.out.println();
+        System.out.println("Enjoy your passphrase - be safe!");
+        System.out.println();
+
 
         // exit. thanks for the Lambda, java 8!
         SpringApplication.exit(ctx, (ExitCodeGenerator) () -> 0);
@@ -53,22 +57,25 @@ public class DicewareCommandLineRunner implements CommandLineRunner {
 
             System.out.print("How many words in the passphrase (default 5)? ");
 
+            // assuming they want the default
             String line = scanner.nextLine();
             if (line.isEmpty()) {
                 return words;
-            } else {
-                // handle NumberFormatException and illegal args
-                try {
-                  int length = Integer.valueOf(line);
-                  if (length < 3 || length > 10) {
-                    System.out.println("Valid values must be between 3 & 10");
-                    return promptUser();
-                  }
-                    return length;
-                } catch (NumberFormatException nfe) {
-                    System.out.println("Not a valid value");
-                    return promptUser();
-                }
+            }
+
+            // handle NumberFormatException and illegal args
+            try {
+              int length = Integer.valueOf(line);
+
+              if (length < 3 || length > 10) {
+                System.out.println("Valid values must be between 3 & 10");
+                return promptUser();
+              }
+              return length;
+
+            } catch (NumberFormatException nfe) {
+              System.out.println("Not a valid value");
+              return promptUser();
             }
         }
     }
