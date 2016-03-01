0eb5e457dd4468996a4d7f988ec3a46ccbb9462f
Kenneth Bowen <kenneth@kennethbowen.com>
2016-02-29 17:52:14 -0700

    Remove duped function.

--- diceware-web/src/main/resources/static/js/diceware-ms.js
+++ diceware-web/src/main/resources/static/js/diceware-ms.js
@@ -1,17 +1,23 @@
 var dicewareMS = angular.module('dicewareMS', []);
 
 dicewareMS.controller('dicewareController', ['$scope', '$http', function($scope, $http) {
-      
-      $http.get('http://localhost:8080/diceware/passphrase')
-        .success(function(data, status, headers, config) {
-            $scope.passphrase = data.passphrase;
-          });
 
-      $scope.getPassphrase = function(length) {
+      /*
+       * Get a passphrase from the diceware web service.
+       */
+      var getNewPassphrase = function(length) {
         $http.get('http://localhost:8080/diceware/passphrase?length=' + length)
-        .success(function(data, status, headers, config) {
-            $scope.passphrase = data.passphrase;
-          });
+          .success(function(data, status, headers, config) {
+              $scope.passphrase =  data.passphrase;
+            });
+      };
+
+      /* gets called when the page get loaded */
+      getNewPassphrase(7);
+
+      /* get a passphrase when choosing a new length */
+      $scope.getPassphrase = function(length) {
+        getNewPassphrase(length);
       };
 }]);
 
