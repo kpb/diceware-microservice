c854f346f6fe70d142a0f5124c28f413498f65da
Kenneth Bowen <hello@kennethbowen.com>
2018-12-07 13:48:18 -0700

    Build diceware-web using gradle

--- diceware-web/src/main/java/com/cultureofcode/diceware/DicewareWebApp.java
+++ diceware-web/src/main/java/com/cultureofcode/diceware/DicewareWebApp.java
@@ -4,7 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
