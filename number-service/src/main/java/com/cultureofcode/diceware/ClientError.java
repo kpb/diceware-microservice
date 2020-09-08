7450c67d8f63f44ad5f62ed9ba9edc33530ed390
Kenneth Bowen <kenneth@kennethbowen.com>
2020-09-08 17:46:02 -0600

    Remove bogus headers

--- number-service/src/main/java/com/cultureofcode/diceware/ClientError.java
+++ number-service/src/main/java/com/cultureofcode/diceware/ClientError.java
@@ -1,14 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.cultureofcode.diceware;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 /**
  * Simple client error dto
+ *
  * @author kenneth
  */
 public class ClientError {
