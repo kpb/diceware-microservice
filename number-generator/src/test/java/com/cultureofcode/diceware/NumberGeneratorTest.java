fe011f3ce48cd79d6f02c99b63b052710b810189
Kenneth Bowen <kenneth@kennethbowen.com>
2023-02-11 15:55:51 +0000

    Tidy Number generator
    - format code & trim whitespace
    - rename tests using Osherve style
    - use better assertions where possible/easy

--- number-generator/src/test/java/com/cultureofcode/diceware/NumberGeneratorTest.java
+++ number-generator/src/test/java/com/cultureofcode/diceware/NumberGeneratorTest.java
@@ -2,31 +2,36 @@ package com.cultureofcode.diceware;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.fail;
-
 import java.util.Arrays;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-// TODO better test method names
 public class NumberGeneratorTest {
+
+  // class under test
+  NumberGenerator numberGenerator;
 
   private int min = 11111;
   private int max = 66666;
 
-  @Test
-  public void generate() {
+  @BeforeEach
+  void beforeEach() {
+    numberGenerator = new NumberGenerator();
+  }
 
-    NumberGenerator numberGenerator = new NumberGenerator();
+  @Test
+  public void generate_10kNumbers_betweenMinandMax() {
 
     for (int i = 0; i < 10000; i++) {
       Integer diceNum = numberGenerator.generate();
-      assertThat(diceNum).as("Generated num greater than or equal to 11111").isGreaterThanOrEqualTo(min);
+      assertThat(diceNum).as("Generated num greater than or equal to 11111")
+          .isGreaterThanOrEqualTo(min);
       assertThat(diceNum).as("Generated num less than or equal to 66666").isLessThanOrEqualTo(max);
     }
   }
 
   @Test
-  public void generateValid() {
-    NumberGenerator numberGenerator = new NumberGenerator();
+  public void generate_10kNumbers_validDiceNumbers() {
 
     for (int i = 0; i < 10000; i++) {
       Integer diceNum = numberGenerator.generate();
@@ -37,43 +42,36 @@ public class NumberGeneratorTest {
   }
 
   @Test
-  public void generateSet() {
-
-    NumberGenerator numberGenerator = new NumberGenerator();
+  public void generate_setsOfNumbers_correctSize() {
 
     int[] setSize = {5, 6, 7, 8, 9, 10};
     Arrays.stream(setSize).forEach(i -> {
-      assertThat(numberGenerator.generate(i).size()).as("Check size of generated number").isEqualTo(i);
+      assertThat(numberGenerator.generate(i).size()).as("Check size of generated number")
+          .isEqualTo(i);
     });
   }
 
-  public void generateSetTooBig() {
+  public void generate_ridiculousNumber_throwsIllegalArgException() {
 
-    NumberGenerator numberGenerator = new NumberGenerator();
     // can you imagine?
     assertThatExceptionOfType(IllegalArgumentException.class)
-    .isThrownBy(() -> numberGenerator.generate(55556));
+        .isThrownBy(() -> numberGenerator.generate(55556));
   }
 
-  public void generateSetTooSmall() {
+  public void generate_toFewNumbers_throwsIllegalArgException() {
 
-    NumberGenerator numberGenerator = new NumberGenerator();
     // generator pleaze...
     assertThatExceptionOfType(IllegalArgumentException.class)
-      .isThrownBy(() -> numberGenerator.generate(1));
+        .as("It doesn't make sense to generate fewer than 3 numbers")
+        .isThrownBy(() -> numberGenerator.generate(1));
   }
 
   @Test
-  public void throwDice() {
+  public void throwDice_100kTimes_alwaysBetween1and6() {
 
-    NumberGenerator numberGenerator = new NumberGenerator();
-
-    for (int i = 0; i < 10000; i++) {
+    for (int i = 0; i < 100000; i++) {
       int diceNum = numberGenerator.throwDice();
-      if (diceNum < 1 || diceNum > 6) {
-        fail("Dice throw is out of range " + diceNum);
-      }
+      assertThat(diceNum).isBetween(1, 6);
     }
   }
-
 }
