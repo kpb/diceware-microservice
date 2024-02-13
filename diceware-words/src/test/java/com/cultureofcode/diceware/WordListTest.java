2b9936aa332529b380662e8d2112ceef14b3f460
Kenneth Bowen <kenneth@kennethbowen.com>
2024-02-13 23:52:20 +0000

    Add additional wordlist file for testing
    Add the Reinhold list, which includes additional formatting, for testing
    parsing the word list.

--- diceware-words/src/test/java/com/cultureofcode/diceware/WordListTest.java
+++ diceware-words/src/test/java/com/cultureofcode/diceware/WordListTest.java
@@ -1,6 +1,8 @@
 package com.cultureofcode.diceware;
 
 import static org.assertj.core.api.Assertions.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import org.junit.jupiter.api.Test;
 
 /**
@@ -13,10 +15,21 @@ public class WordListTest {
    * Test of load method, of class WordList.
    */
   @Test
-  public void testLoad() {
+  public void NewInstance_DefaultWordList_7776Entries() {
 
     WordList wordList = WordList.newInstance();
-    assertThat(wordList.getDicewordMap().size()).as("Word list containts 7776 entries")
-        .isEqualTo(7776);
+    assertThat(wordList.getDicewordMap()).as("Word list containts 7776 entries")
+        .hasSize(7776);
+  }
+
+  @Test
+  public void ParseWordlistLines_ReiholdList_7776Entries() {
+
+    WordList wordList = new WordList();
+
+    BufferedReader reader = new BufferedReader(
+        new InputStreamReader(this.getClass().getResourceAsStream("/diceware.wordlist.asc")));
+    assertThat(wordList.parseWordlistLines(reader.lines()))
+        .as("Word list containts 7776 entries").hasSize(7776);
   }
 }
